580d90f9335c51f78f69e8280c2ca15a030eee2f
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-05-06 18:23:34 +0200

    Target html elements

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const API_KEY = "7e06685fd85ca11f85cd3e8a7eb7e12f"
 const image_path = "https://image.tmdb.org/t/p/w1280"
 const trailer_path = "https://www.youtube.com/watch?v="
+
+const input = document.querySelector(".search input");
+const button = document.querySelector(".search button");
+const mainGridTitle = document.querySelector(".favorites h1");
+const mainGrid = document.querySelector(".favorites .movies-grid");
+
