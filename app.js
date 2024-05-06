a6148f6928c31059d5cfaf27d1622155e09782c5
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-05-06 18:56:37 +0200

    Specify fetch data with innerhtml

--- app.js
+++ app.js
@@ -19,5 +19,25 @@ async function addSearchedMovies(){
 	const data = await getMovieBySearch(input.value)
 	console.log(data);
 
-	
+	mainGridTitle.innerText = "Search Results..."
+	mainGrid.innerHTML = data.map(e =>{
+		return `
+				<div class="card" data-id="${e.id}">
+					<div class="img">
+					<img src="${image_path + e.poster_path}">
+				</div>
+					<div class="info">
+					<h2>${e.title}</h2>
+					<div class="single-info">
+						<span>Rating: </span>
+						<span>10 / 10</span>
+					</div>
+				<div class="single-info">
+					<span>Release date: </span>
+					<span>10-04-2024</span>
+				</div>
+			</div>
+		</div>
+		`
+	}).join("")
 }
