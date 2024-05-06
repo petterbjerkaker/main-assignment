8041d9652b4e1a582d07ece9389f31cdd31ae628
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-05-06 18:36:09 +0200

    Initialize async fetch function and api key

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ const button = document.querySelector(".search button");
 const mainGridTitle = document.querySelector(".favorites h1");
 const mainGrid = document.querySelector(".favorites .movies-grid");
 
+async function getMovieBySearch (search_term){
+	const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search_term}`);
+	const responseData = await response.json();
+	console.log(responseData.results);
+	return responseData.results;
+}
+getMovieBySearch("game");
