43c8b148890597dc3a06c1a014a7f540ba84c84d
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-05-10 11:53:08 +0200

    Import firebase/app and /auth

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+
+const firebaseApp = initializeApp({
+});
+const auth = getAuth(firebaseApp);
+
+
+
+//DETECT AUTH STATE
+onAuthStateChanged(auth, user => {
+	if(user != null){
+		console.log("Logged in!");
+	}else{
+		console.log("No user");
+	}
+});
