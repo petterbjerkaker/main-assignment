e2f19e5765aa5562e7dde92aa178977823d826c6
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-05-10 14:58:19 +0200

    Add firebaseConfig

---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBJlHyaeU53R1gonwEHSH23D1qDDSvC8wg",
+	authDomain: "main-assignment-990e8.firebaseapp.com",
+	projectId: "main-assignment-990e8",
+	storageBucket: "main-assignment-990e8.appspot.com",
+	messagingSenderId: "255805880148",
+	appId: "1:255805880148:web:f6817f7cd6e2c969123ac1"
+ };
+
+ export default firebaseConfig;
