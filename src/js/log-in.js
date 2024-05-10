cfbde422b728e3e72657e4672c335d2b642361a5
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-05-10 11:52:40 +0200

    Selecting log in elements

--- src/js/log-in.js
+++ src/js/log-in.js
@@ -1,20 +1,75 @@
-const validateLogInForm = (email, password, emailError, passError)=>{
+//SELECTING THE LOG-IN FORM ELEMENTS
+const emailInput = document.querySelector(".email");
+const passwordInput = document.querySelector(".password");
+const logInButton = document.querySelector(".log-in_button");
+const emailError = document.querySelector(".email-error");
+const passwordError = document.querySelector(".password-error");
+const logInForm = document.querySelector(".log-in_form");
+const logInError = document.querySelector(".log-in_error");
+
+
+
+//VALIDATION FOR LOG IN FORM FUNCTION
+const validateLogInForm = (email, password, emailErrorElement, passErrorElement)=>{
 	const errors = {
 		errorStatus: false,
-		emailError: "",
+		emailErrorElement: "",
 		passwordError: "",
 	};
 
 	if(!email && !password){
 		errors.errorStatus = true,
-		errors.emailError = "Email is required ⚠️",
+		errors.emailErrorElement = "Email is required ⚠️",
 		errors.passwordError = "Password is required ⚠️";
 
-		emailError.style.visibility = "visible";
-		passError.style.visibility = "visible";
+		emailErrorElement.style.visibility = "visible";
+		passErrorElement.style.visibility = "visible";
 
 
-		emailError.textContent = errors.emailError;
-		passError.textContent = errors.passwordError;
+		emailErrorElement.textContent = errors.emailErrorElement;
+		passErrorElement.textContent = errors.passwordError;
+
+	}else if(!email){
+		errors.errorStatus = true,
+		errors.emailErrorElement = "Email is required ⚠️",
+		errors.passwordError = "";
+
+		emailErrorElement.style.visibility = "visible";
+		passErrorElement.style.visibility = "hidden";
+
+
+		emailErrorElement.textContent = errors.emailErrorElement;
+		passErrorElement.textContent = errors.passwordError;
+
+	}else if(!password){
+		errors.errorStatus = true,
+		errors.emailErrorElement = "",
+		errors.passwordError = "Password is required ⚠️";
+
+		emailErrorElement.style.visibility = "hidden";
+		passErrorElement.style.visibility = "visible";
+
+
+		emailErrorElement.textContent = errors.emailErrorElement;
+		passErrorElement.textContent = errors.passwordError;
+
+	}else{
+		errors.errorStatus = false,
+		errors.emailErrorElement = "",
+		errors.passwordError = "";
+
+		emailErrorElement.style.visibility = "hidden";
+		passErrorElement.style.visibility = "hidden";
+
+
+		emailErrorElement.textContent = errors.emailErrorElement;
+		passErrorElement.textContent = errors.passwordError;
 	}
+
+	const logInFormStatus = ()=>{
+		return errors.errorStatus
+	}
+
+	return { logInFormStatus }
 };
+
