b6256c8ea886c0f888485ac28f2309caed279e3d
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-05-10 13:35:02 +0200

    Validate register form and register button

--- src/js/register.js
+++ src/js/register.js
@@ -4,3 +4,31 @@ const registerEmail = document.querySelector(".email");
 const registerPassword = document.querySelector(".password");
 const registerError = document.querySelector(".register-error");
 const registerForm = document.querySelector(".register-form");
+const registerButton = document.querySelector(".register-button");
+
+
+//VALIDATE REGISTER FORM
+const validateRegisterForm = (firstname, lastname, email, password, registerError)=>{
+	let errorStatus = false;
+	if(!firstname || !lastname || !email || !password ){
+		errorStatus = true;
+		registerError.style.visibility = "visible";
+		registerError.textContent = "Please fill out all the fields!"
+	}else{
+		errorStatus = false;
+		registerError.style.visibility = "hidden";
+		registerError.textContent = "";
+	}
+	const registerErrorStatus = ()=>{
+		return errorStatus;
+	}
+	return {registerErrorStatus}
+	
+};
+
+//VALIDATION REGISTER BUTTON
+registerButton.addEventListener("click", (e)=>{
+	e.preventDefault();
+	validateRegisterForm(registerFirstname.value, registerLastname.value, registerEmail.value, registerPassword.value, registerError)
+});
+
