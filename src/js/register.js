59b0ae6131a9c891b8c9777fd568b97ba65f84fa
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-05-10 12:52:39 +0200

    Selecting elements from register form

--- src/js/register.js
+++ src/js/register.js
@@ -2,3 +2,5 @@ const registerFirstname = document.querySelector(".firstname");
 const registerLastname = document.querySelector(".lastname");
 const registerEmail = document.querySelector(".email");
 const registerPassword = document.querySelector(".password");
+const registerError = document.querySelector(".register-error");
+const registerForm = document.querySelector(".register-form");
